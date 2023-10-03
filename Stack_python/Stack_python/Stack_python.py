e3ba285e0f0c88cb1f33b50d03de252f4ed5064c
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-10-03 11:43:48 +0300

    Eng Version

--- Stack_python/Stack_python/Stack_python.py
+++ Stack_python/Stack_python/Stack_python.py
@@ -1,23 +1,23 @@
-# Стек (очередь) - структура данных, в которой можно либо добавлять в начало, либо удалять из начала, как в стопке тарелок
-# Push(item) добавляет элемент в начало
-# Pop() убирает элемент из начала
+# Stack - data type that can do two commands Pop() and Push() 
+# Push(item) - creating last element 
+# Pop() - deleting last element 
 class Stack:
-    # Инициализация объекта при создании
+    # Initialization
     def __init__(self):
         self.items=[]
-    # Проверка на пустоту
+    # CHeck for null
     def isEmpty(self):
         return len(self.items) == 0
-    # Запись в начало
+    # Creating last object
     def push(self, item):
         self.items.append(item)
-    # Удаление начала
+    # Delete last object
     def pop(self):
         if not self.isEmpty():
             self.items.pop()
         else:
             print('ЕГГОГ')
-    # Размер стека
+    # Size of stack
     def size(self):
         return len(self.items)
 # Main
@@ -25,7 +25,7 @@ st = Stack()
 st.push(1)
 st.push(2)
 st.push(3)
-print('Что хотите сделать? 1:pop 2:push')
+print('What do you want to do? 1:pop 2:push')
 what = int(input())
 if what == 1:
     st.pop()
@@ -33,7 +33,7 @@ if what == 2:
     a=int(input())
     st.push(a)
 print(st.items)
-print(f'Размер: {st.size()}')
+print(f'Size: {st.size()}')
 
 
 
