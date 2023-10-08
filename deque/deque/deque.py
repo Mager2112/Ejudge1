3a90eb6fd810cef0eb4e81589f5ec6750c20a350
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-10-08 13:41:59 +0300

    Update deque.py

--- deque/deque/deque.py
+++ deque/deque/deque.py
@@ -1,40 +1,29 @@
+#Deque - data structure, where you can add and delete elements at left and right
 class Deque:
+    #initialization
     def __init__(self):
         self.items = []
-
+    #check for emptiness
     def is_empty(self):
         return self.items == []
-
+    #add in left
     def append_left(self, item):
         self.items.insert(0, item)
-
+    #add in right
     def append_right(self, item):
         self.items.append(item)
-
+    #delete in left
     def pop_left(self):
         if not self.is_empty():
             return self.items.pop(0)
         else:
             return "Deque is empty"
-
+    #delete in right
     def pop_right(self):
         if not self.is_empty():
             return self.items.pop()
         else:
             return "Deque is empty"
-
-    def peek_left(self):
-        if not self.is_empty():
-            return self.items[0]
-        else:
-            return "Deque is empty"
-
-    def peek_right(self):
-        if not self.is_empty():
-            return self.items[-1]
-        else:
-            return "Deque is empty"
-
     def size(self):
         return len(self.items)
 if __name__ == '__main__':
